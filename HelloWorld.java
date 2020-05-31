2e292d0cf5ed509f8dad51844107dac1da30e6f8
Shivam Pandey <shivamraj472@gmail.com>
2020-05-31 14:38:20 +0530

    print statement changed

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello World");
+    System.out.println("Hello World");
+    System.out.println("divya is mad");
   }
 }
